985dfa44d640d021f4fad9c5ad7c97e8590e6716
Peter Erickson <poe9514@gmail.com>
2010-01-04 17:09:32 -0500

    Added game timer -- tested and workin.  More work on the level list.

--- src/edu/rit/poe/atomix/R.java
+++ src/edu/rit/poe/atomix/R.java
@@ -38,9 +38,9 @@ public final class R {
         public static final int wall_vertical_top_end=0x7f020018;
     }
     public static final class id {
-        public static final int atomix_title=0x7f050010;
-        public static final int bglayout=0x7f05000f;
-        public static final int continue_button=0x7f050012;
+        public static final int atomix_title=0x7f050011;
+        public static final int bglayout=0x7f050010;
+        public static final int continue_button=0x7f050013;
         public static final int continue_dialog=0x7f050000;
         public static final int detailstable=0x7f050003;
         public static final int goal_details_solution=0x7f05000a;
@@ -48,17 +48,18 @@ public final class R {
         public static final int goal_dialog2=0x7f050007;
         public static final int goal_dialog_atomix_image=0x7f050008;
         public static final int goal_dialog_level=0x7f050009;
-        public static final int help_button=0x7f050014;
-        public static final int icon=0x7f05000e;
-        public static final int level_completed=0x7f05000c;
+        public static final int help_button=0x7f050015;
+        public static final int icon=0x7f05000f;
+        public static final int level_completed_moves=0x7f05000e;
+        public static final int level_completed_seconds=0x7f05000c;
         public static final int level_name=0x7f05000d;
         public static final int level_number=0x7f05000b;
-        public static final int main_button_list=0x7f050011;
-        public static final int new_game_button=0x7f050013;
+        public static final int main_button_list=0x7f050012;
+        public static final int new_game_button=0x7f050014;
         public static final int new_name=0x7f050004;
         public static final int newgame_dialog=0x7f050002;
         public static final int play_button=0x7f050005;
-        public static final int quit_button=0x7f050015;
+        public static final int quit_button=0x7f050016;
         public static final int saved_games_list=0x7f050001;
     }
     public static final class layout {
@@ -71,6 +72,13 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int completed_moves_text=0x7f040004;
+        /**  Strings for the Levels List 
+         */
+        public static final int completed_seconds_text=0x7f040003;
+        /**  Activity Titles 
+         */
         public static final int home_title=0x7f040001;
+        public static final int levels_title=0x7f040002;
     }
 }
